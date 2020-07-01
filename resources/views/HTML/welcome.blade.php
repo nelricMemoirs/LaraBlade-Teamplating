99c3efb9d41307dc4193e5b0321223b46ea9b685
Arief Luqman <bookswithlov3@gmail.com>
2020-07-02 03:28:20 +0700

    removed HTML folder

--- resources/views/HTML/welcome.blade.php
+++
@@ -1,8 +0,0 @@
-@extends('layout.app')
-
-@section('content')
-    <h1>Selamat Datang {{ ucfirst($namadepan) . ' '. ucfirst($namabelakang) }} di LaraTest</h1>
-    
-    <h4>Terima kasih telah bergabung di Sanberbook. Social Media kita bersama!</h4>
-
-@endsection
